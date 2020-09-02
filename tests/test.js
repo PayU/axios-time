7e439163d0b5bcad3ebd772d57f1bca447ded677
omer <omer.sher@zooz.com>
2020-09-02 10:45:21 +0300

    add negative tests

--- tests/test.js
+++ tests/test.js
@@ -7,10 +7,26 @@ const expect = require('chai').expect;
 axiosTime(axios);
 
 describe('testing axios-time package', () => {
-    it('should have described timing properties correctly', async () => {
-        const response = await axios.get('http://ynet.co.il');
+    it('should have described timing properties correctly on success', async () => {
+        const response = await axios.get('https://httpstat.us/200');
         expect(response.timings).to.have.property('timingEnd').that.is.a('number');
         expect(response.timings).to.have.property('timingStart').that.is.a('number');
         expect(response.timings).to.have.property('elapsedTime').that.is.a('number');
+    });
+    it('should have described timing properties correctly on error', async () => {
+        try {
+            await axios.get('https://httpstat.us/500');
+        } catch (err) {
+            expect(err.response.timings).to.have.property('timingEnd').that.is.a('number');
+            expect(err.response.timings).to.have.property('timingStart').that.is.a('number');
+            expect(err.response.timings).to.have.property('elapsedTime').that.is.a('number');
+        }
+    });
+    it('should not have response on client error', async () => {
+        try {
+            await axios.get('127.0.0.1');
+        } catch (err) {
+            expect(err.response).to.eql(undefined);
+        }
     });
 });
